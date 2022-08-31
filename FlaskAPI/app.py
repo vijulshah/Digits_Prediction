378f19a6f95b7731e21dea877c1cb56ebf52dd36
vijulshah <vijul1904@gmail.com>
2022-09-01 01:25:30 +0200

    FrondEnd and Backend Integrated with Machine Learning Model

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,30 +1,44 @@
-import flask
-from flask import Flask, jsonify, request
+from flask import Flask, request
+from flask_cors import CORS
 import json
-from data_input import data_in
 import numpy as np
-import pickle
+from torchvision import transforms
+import torch
+from PIL import Image
+import torch.nn.functional as F
 
-def load_models():
-    file_name = "models/MNIST_Digit_Prediction_Model"
-    with open(file_name, 'rb') as pickled:
-        data = pickle.load(pickled)
-        model = data['model']
-    return model
+def predict_real_img(image_file, model):
+    img = Image.open(image_file).convert(mode="L")
+    img = img.resize((28, 28))
+    # x = (255 - np.expand_dims(np.array(img), -1))/255.
+    x = (np.expand_dims(np.array(img), -1))/255.
+    with torch.no_grad():
+        T = transforms.Compose([
+            transforms.ToTensor()
+        ])
+        pred = model(torch.unsqueeze(T(x), axis=0).float())
+        return F.softmax(pred, dim=-1).numpy()
 
 app = Flask(__name__)
-@app.route('/predict', methods=['GET'])
+CORS(app)
+
+@app.route('/predict-digit-from-image', methods=['POST'])
 def predict():
-    # stub input features
-    request_json = request.get_json()
-    x = request_json['input']
-    # print(x)
-    x_in = np.array(x).reshape(1,-1)
-    # load model
-    model = load_models()
-    prediction = model.predict(x_in)[0]
-    response = json.dumps({'response': prediction})
-    return response, 200
+    print("request files", request.files)
+    image_file = request.files['image']
+    
+    digits_model = torch.load("models/MNIST_Digit_Prediction_Model")
+    pred = predict_real_img(image_file, digits_model)
+    pred_idx = np.argmax(pred)
+    prob = "{:.2f}".format(pred[0][pred_idx]*100)
+    print(f"predicted: {pred_idx}, prob: {prob} %")
+
+    responseCode = 200
+    responseData = json.dumps({
+        'predicted_number': str(pred_idx),
+        'prediction_probability': f"{prob} %"
+    })
+    return responseData, responseCode
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    app.run(debug=True)
