089bb61ba145d3888c9b97881ea0764691c15d7c
vijulshah <vijul1904@gmail.com>
2022-09-01 11:39:15 +0200

    UI Changes and readme added

--- react-flask-app/src/Home/PredictDigitFromImage.js
+++ react-flask-app/src/Home/PredictDigitFromImage.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Endpoints from '../API/api.js';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import Spinner from 'react-bootstrap/Spinner';
 
 class PredictDigitFromImage extends Component {
 
@@ -17,23 +20,42 @@ class PredictDigitFromImage extends Component {
 
     render(){
         return(
-            <div>
-                <h3>Predict Digit From Image</h3>
-                <span>(Works best with black background)</span><br/><br/><br/>
+            <Container>
+                <Row style={{ marginTop: '10px' }}>
+                    <Col style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                        <h2>Predict Digit From Image</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                        <span>(Works best with black background)</span>
+                    </Col>
+                </Row>
+                <br/>
+                <div>
                 {
                     this.state.uploadedDigitImage !== "" ?
-                    <div>
-                        <img 
-                            alt="uploaded image preview" 
-                            src={this.state.previewImg} 
-                            height="300px"
-                            width="300px"
-                        />
-                        <br/>
-                    </div>
+                    <Row>
+                        <Col style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                            <img 
+                                alt="uploaded image preview" 
+                                src={this.state.previewImg} 
+                                height="300px"
+                                width="300px"
+                                style={{ borderRadius: '15px', boxShadow: '2px 2px #888888' }}
+                            />
+                        </Col>
+                    </Row>
                     :
-                    null
+                    <Row>
+                        <Col style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                            <div style={{ display: 'flex', height: "300px", width: "300px", backgroundColor: 'lightgray', opacity: '50%', borderRadius: '15px', alignItems: 'center', justifyContent: 'center', }}>
+                                Upload Image
+                            </div>
+                        </Col>
+                    </Row>
                 }
+                <br/>
                 <input 
                     type="file" 
                     style={{display: 'none'}}
@@ -41,43 +63,75 @@ class PredictDigitFromImage extends Component {
                     ref={(fileInput) => this.fileInput = fileInput}
                     onChange = {(event) => this.handleChange(event)}
                 />
-                <button
-                    onClick={() => this.fileInput.click()}
-                >
-                    Pick Image
-                </button>
-                <br/><br/>
-                <button 
-                    style={{marginRight: "10px"}}
-                    disabled={this.state.errorMessage!=="" || this.state.uploadedDigitImage === ""}
-                    onClick={() => this.getPrediction()}
-                >
-                    Predict
-                </button>
-                <button 
-                    style={{marginLeft: "10px"}}
-                    onClick={() => this.clearBtn()}
-                >
-                    Clear
-                </button>
+                <Row style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                    <Col style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                        <button
+                            onClick={() => this.fileInput.click()}
+                            style={{ borderRadius: '5px', backgroundColor: 'orange', border: 'none', padding: '10px', boxShadow: '2px 2px #888888', width: '200px', color: "black" }}
+                        >
+                            Pick Image
+                        </button>
+                    </Col>
+                    <Col style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                    {
+                        this.state.errorMessage!=="" || this.state.uploadedDigitImage === "" ?
+                        <button
+                            style={{ borderRadius: '5px', backgroundColor: 'lightblue', opacity: '50%', border: 'none', padding: '10px', boxShadow: '2px 2px #888888', width: '200px', color: 'darkblue' }}
+                            disabled={true}
+                        >
+                            Predict
+                        </button>
+                        :
+                        <button
+                            style={{ borderRadius: '5px', backgroundColor: 'lightblue', border: 'none', padding: '10px', boxShadow: '2px 2px #888888', width: '200px', color: 'darkblue' }}
+                            disabled={this.state.errorMessage!=="" || this.state.uploadedDigitImage === ""}
+                            onClick={() => this.getPrediction()}
+                        >
+                            Predict
+                        </button>
+                    }
+                    </Col>
+                    <Col style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                        <button 
+                            style={{ borderRadius: '5px', backgroundColor: 'lightgreen', border: 'none', padding: '10px', boxShadow: '2px 2px #888888', width: '200px', color: 'darkgreen' }}
+                            onClick={() => this.clearBtn()}
+                        >
+                            Clear
+                        </button>
+                    </Col>
+                </Row>
                 <br/><br/>
                 {
-                    this.state.errorMessage ? 
-                    <span style={{color: "red"}}>
-                        {this.state.errorMessage}
-                    </span>
+                    this.state.isPredicting ?
+                    <Row>
+                        <Col style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                            <Spinner animation="border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </Spinner>
+                        </Col>
+                    </Row>
                     :
-                    null
+                    <Row>
+                        {
+                            this.state.errorMessage ? 
+                            <Col style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', color: "red", fontSize: '25px' }}>
+                                <b style={{ marginRight: '5px' }}>Error: </b>{this.state.errorMessage}
+                            </Col>
+                            :
+                            null
+                        }
+                        {
+                            this.state.successMessage ? 
+                            <Col style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', color: "green", fontSize: '25px' }}>
+                                <b style={{ marginRight: '5px' }}>Prediction: </b>{this.state.successMessage}
+                            </Col>
+                            :
+                            null
+                        }
+                    </Row>
                 }
-                {
-                    this.state.successMessage ? 
-                    <span style={{color: "green"}}>
-                        {this.state.successMessage}
-                    </span>
-                    :
-                    null
-                }
-            </div>
+                </div>
+            </Container>
         );
     }
 
@@ -103,11 +157,15 @@ class PredictDigitFromImage extends Component {
         let url = Endpoints.home.predictDigitFromImage;
         var formData = new FormData();
         formData.append("image", this.state.uploadedDigitImage)
+        this.setState({
+            isPredicting: true
+        });
         axios.post(url, formData)
         .then((response) => {
             if(Endpoints.showLogsOnConsole) console.log("response = ",response);
             this.setState({
-                successMessage: "Predicted Digit: "+ response.data.predicted_number
+                successMessage: "The Predicted Digit is "+ response.data.predicted_number,
+                isPredicting: false
             })
         })
         .catch((error) => {
@@ -116,13 +174,6 @@ class PredictDigitFromImage extends Component {
                 isPredicting: false,
                 errorMessage: "Something Went Wrong! Please try by uploading a different image or Try again later",
                 successMessage: ""
-            },()=>{
-                setTimeout(()=>{
-                    this.setState({
-                        errorMessage: "",
-                        successMessage: ""
-                    });
-                },5000)
             });
         })
     }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Steps to run server and client sides:
+
+- Open two terminals. 
+- In 1st one - go into <b>`FlaskAPI`</b> folder and type <b>`pip install -r requirements.txt`</b> to get all the required python packages and then type <b>`python wsgi.py`</b> to start the server.
+- In 2nd one - go into <b>`react-flask-app`</b> and type <b>`npm install`</b> to install all the required node_modules and then type <b>`npm start`</b> to start the client.
+
+# Working of the model:
+- Click on <b>`Pick Image`</b> and upload any image (Preferably, digit with black background - as the model is trained for such images).
+- Press <b>`Predict`</b> and you will get the results just below.
